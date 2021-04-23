cf07325461728731411f5e900e644d9650e29816
Xin Ni <oonxinni@gmail.com>
2021-04-23 14:18:26 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
-import "calc"
+// import "calc"
 
 func main() {
-	calc.Add(2, 3)
-	calc.Substract(2, 3)
-	calc.Divide(2, 3)
-	calc.Multiply(2, 3)
+// 	calc.Add(2, 3)
+// 	calc.Substract(2, 3)
+// 	calc.Divide(2, 3)
+// 	calc.Multiply(2, 3)
 }
